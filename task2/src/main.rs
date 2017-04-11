ea53b367f8a0ffe29cfbff222bbc25487a2f785b
nm <nm@localhost.localdomain>
2017-04-11 21:59:56 +0300

    Add plot example

--- task2/src/main.rs
+++ task2/src/main.rs
@@ -1,7 +1,10 @@
 extern crate nalgebra as na;
+extern crate gnuplot;
 
 use na::core::{DMatrix, MatrixVec};
 use std::f64;
+
+use gnuplot::*;
 
 fn polynomial_error(x0: f64, pts: &Vec<f64>) -> f64 {
     let mut res = 0.0;
@@ -69,8 +72,8 @@ fn main() {
     let u_grid = linspace_vec(-1.0, 1.0, N); // uniform grid
     //let c_grid = Vec::new();                 // Chebyshev grid
     let mut base = Vec::with_capacity(pts.len());
-    for x in pts {
-        base.push(polynomial_error(x, &u_grid));
+    for x in pts.iter() {
+        base.push(polynomial_error(*x, &u_grid));
     }
     println!("{:?}", base);
 
@@ -79,4 +82,24 @@ fn main() {
     println!("Hello, world!");
     println!("{:?}", linspace(-1.0, 1.0, 4));
     println!("{:?}", chebyshev_knots(5));
+
+    let mut fg = Figure::new();
+
+    fg.axes2d()
+        .set_size(0.75, 1.0)
+        .set_title("Example Plot fg1.1", &[])
+        .set_x_ticks(Some((Fix(1.0), 1)), &[Mirror(false)], &[])
+        .set_y_ticks(Some((Fix(1.0), 1)), &[Mirror(false)], &[])
+        .set_legend(Graph(1.0), Graph(0.5), &[Placement(AlignLeft, AlignCenter)], &[TextAlign(AlignRight)])
+        .set_border(true, &[Left, Bottom], &[LineWidth(2.0)])
+        .set_x_label("Abscissa", &[])
+        .set_y_label("Ordinate", &[])
+        .lines(&pts, &base, &[Caption("base"), LineWidth(1.5), Color("black")]);
+
+
+    fg.set_terminal("pngcairo", "fg1.1.png");
+    fg.show();
 }
+
+
+
