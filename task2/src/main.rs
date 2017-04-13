53ee60842051ca18eef3dd3f436fbaf258dd0e7c
nm <idvy94@gmail.com>
2017-04-13 21:40:23 +0300

    Update task2

--- task2/src/main.rs
+++ task2/src/main.rs
@@ -1,7 +1,5 @@
-extern crate nalgebra as na;
 extern crate gnuplot;
 
-use na::core::{DMatrix, MatrixVec};
 use std::f64;
 
 use gnuplot::*;
@@ -35,7 +33,7 @@ fn polynomial_derivative_error(x0: f64, pts: &Vec<f64>) -> f64 {
     res
 }
 
-fn linspace_vec(min: f64, max: f64, num: usize) -> Vec<f64> {
+fn linspace(min: f64, max: f64, num: usize) -> Vec<f64> {
     let dt = (max - min) / ((num - 1) as f64);
     let mut pts = Vec::with_capacity(num);
     for i in 0..num {
@@ -55,50 +53,39 @@ fn chebyshev_knots(k: usize) -> Vec<f64> {
     res
 }
 
-fn linspace(min: f64, max: f64, num: usize) -> DMatrix<f64> {
-    let dt = (max - min) / ((num - 1) as f64);
-    let mut pts = Vec::with_capacity(num);
-    for i in 0..num {
-        pts.push(min + dt * i as f64);
-    }
-
-    DMatrix::from_data(MatrixVec::new(na::Dynamic::new(1), na::Dynamic::new(num), pts))
-}
-
-
-fn main() {
-    let N = 11;
-    let pts = linspace_vec(-1.0, 1.0, 301);
-    let u_grid = linspace_vec(-1.0, 1.0, N); // uniform grid
-    //let c_grid = Vec::new();                 // Chebyshev grid
-    let mut base = Vec::with_capacity(pts.len());
-    for x in pts.iter() {
-        base.push(polynomial_error(*x, &u_grid));
-    }
-    println!("{:?}", base);
-
-
-
-    println!("Hello, world!");
-    println!("{:?}", linspace(-1.0, 1.0, 4));
-    println!("{:?}", chebyshev_knots(5));
-
+fn plot(name: &str, x: &[f64], y: &[f64]) {
     let mut fg = Figure::new();
 
     fg.axes2d()
         .set_size(0.75, 1.0)
-        .set_title("Example Plot fg1.1", &[])
+        .set_title(name, &[])
         .set_x_ticks(Some((Fix(1.0), 1)), &[Mirror(false)], &[])
         .set_y_ticks(Some((Fix(1.0), 1)), &[Mirror(false)], &[])
         .set_legend(Graph(1.0), Graph(0.5), &[Placement(AlignLeft, AlignCenter)], &[TextAlign(AlignRight)])
         .set_border(true, &[Left, Bottom], &[LineWidth(2.0)])
         .set_x_label("Abscissa", &[])
         .set_y_label("Ordinate", &[])
-        .lines(&pts, &base, &[Caption("base"), LineWidth(1.5), Color("black")]);
+        .lines(x, y, &[Caption("base"), LineWidth(1.5), Color("black")]);
 
 
-    fg.set_terminal("pngcairo", "fg1.1.png");
+    fg.set_terminal("pngcairo", &format!("{}.png", name));
     fg.show();
+}
+
+fn main() {
+    let n = 11;
+    let pts = linspace(-1.0, 1.0, 301);
+    let u_grid = linspace(-1.0, 1.0, n); // uniform grid
+    let c_grid = chebyshev_knots(n);                 // Chebyshev grid
+    let mut u_base = Vec::with_capacity(pts.len());
+    let mut c_base = Vec::with_capacity(pts.len());
+    for x in pts.iter() {
+        u_base.push(polynomial_error(*x, &u_grid));
+        c_base.push(polynomial_error(*x, &c_grid));
+    }
+
+    plot("Uniform grid"  , &pts, &u_base);
+    plot("Chebyshev grid", &pts, &c_base);
 }
 
 
