211a359b0876ad847022d19677fb976be701b5db
nm <nm@localhost.localdomain>
2017-04-13 22:02:02 +0300

    Finalize task2, create task3

--- task2/src/main.rs
+++ task2/src/main.rs
@@ -24,10 +24,24 @@ fn polynomial_derivative_error(x0: f64, pts: &Vec<f64>) -> f64 {
     let mut res = 0.0;
 
     for i in 0..pts.len() {
-        let mut t1 = 0.0;
-        let mut t2 = 0.0;
-        assert!(false);
+        let mut tmp = 0.0;
+        for j in 0..pts.len() {
+            let mut tmp2 = 1.0;
+            for k in 0..pts.len() {
+                if k != j {
+                    tmp2 *= x0 - pts[k];
+                }
+            }
+            tmp += tmp2;
+        }
+        let mut tmp3 = 1.0;
+        for j in 0..pts.len() {
+            if j != i {
+                tmp3 *= pts[i] - pts[j];
+            }
+        }
 
+        res += (tmp / tmp3).abs();
     }
 
     res
@@ -53,22 +67,23 @@ fn chebyshev_knots(k: usize) -> Vec<f64> {
     res
 }
 
-fn plot(name: &str, x: &[f64], y: &[f64]) {
+fn plot(plot_name: &str, name1: &str, x1: &[f64], y1: &[f64], name2: &str, x2: &[f64], y2: &[f64]) {
     let mut fg = Figure::new();
 
     fg.axes2d()
         .set_size(0.75, 1.0)
-        .set_title(name, &[])
+        .set_title(plot_name, &[])
         .set_x_ticks(Some((Fix(1.0), 1)), &[Mirror(false)], &[])
-        .set_y_ticks(Some((Fix(1.0), 1)), &[Mirror(false)], &[])
+        .set_y_ticks(Some((Auto, 1)), &[Mirror(false)], &[])
         .set_legend(Graph(1.0), Graph(0.5), &[Placement(AlignLeft, AlignCenter)], &[TextAlign(AlignRight)])
         .set_border(true, &[Left, Bottom], &[LineWidth(2.0)])
         .set_x_label("Abscissa", &[])
         .set_y_label("Ordinate", &[])
-        .lines(x, y, &[Caption("base"), LineWidth(1.5), Color("black")]);
+        .lines(x1, y1, &[Caption(name1), LineWidth(1.5), Color("red")])
+        .lines(x2, y2, &[Caption(name2), LineWidth(1.5), Color("green")]);
 
 
-    fg.set_terminal("pngcairo", &format!("{}.png", name));
+    fg.set_terminal("pngcairo", &format!("{}.png", plot_name));
     fg.show();
 }
 
@@ -79,13 +94,17 @@ fn main() {
     let c_grid = chebyshev_knots(n);                 // Chebyshev grid
     let mut u_base = Vec::with_capacity(pts.len());
     let mut c_base = Vec::with_capacity(pts.len());
+    let mut u_base_der = Vec::with_capacity(pts.len());
+    let mut c_base_der = Vec::with_capacity(pts.len());
     for x in pts.iter() {
         u_base.push(polynomial_error(*x, &u_grid));
         c_base.push(polynomial_error(*x, &c_grid));
+        u_base_der.push(polynomial_derivative_error(*x, &u_grid));
+        c_base_der.push(polynomial_derivative_error(*x, &c_grid));
     }
 
-    plot("Uniform grid"  , &pts, &u_base);
-    plot("Chebyshev grid", &pts, &c_base);
+    plot("Px"  , "Uniform grid", &pts, &u_base, "Chebyshev grid", &pts, &c_base);
+    plot("Px.der"  , "Uniform grid", &pts, &u_base_der, "Chebyshev grid", &pts, &c_base_der);
 }
 
 
