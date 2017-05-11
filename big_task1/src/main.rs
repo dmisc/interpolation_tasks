d3326acb4212c841ca197fe61f19ad26c4a21bc2
a <a@a.a>
2017-05-11 08:40:59 +0300

    Add clamped spline with derivatives

--- big_task1/src/main.rs
+++ big_task1/src/main.rs
@@ -68,6 +68,7 @@ impl CubicSpline {
 // c -- upper
 // d -- result column
 fn solve_tridiagonal(a: &[f64], b: &[f64], c: &[f64], d: &[f64]) -> Vec<f64> {
+    println!("a {}, b {}, c {}, d {}", a.len(), b.len(), c.len(), d.len());
     let mut cm = Vec::new();
     let mut dm = Vec::new();
     cm.push(c[0] / b[0]);
@@ -136,7 +137,7 @@ fn create_cubic_spline_natural(pts: &[(f64, f64)]) -> CubicSpline {
     }
 }
 
-fn create_cubic_spline_clamped(pts: &[(f64, f64)]) -> CubicSpline {
+fn create_cubic_spline_clamped2(pts: &[(f64, f64)]) -> CubicSpline {
     let y = pts.iter().map(|&(_, y)| y).collect::<Vec<_>>();
     let t = pts.iter().map(|&(t, _)| t).collect::<Vec<_>>();
 
@@ -166,6 +167,74 @@ fn create_cubic_spline_clamped(pts: &[(f64, f64)]) -> CubicSpline {
     z.push(0.5 * (6.0 * b[0] / h[0] - res[0]));
     z.extend_from_slice(&res);
     z.push(-0.5 * (6.0 * b.last().unwrap() / h.last().unwrap() + res.last().unwrap()));
+
+    let mut sections = Vec::new();
+
+    for i in 0..z.len() - 1 {
+        sections.push(CubicSection {
+                          c_begin: z[i + 1] / (6.0 * h[i]),
+                          c_end: z[i] / (6.0 * h[i]),
+                          l_begin: y[i + 1] / h[i] - z[i + 1] * h[i] / 6.0,
+                          l_end: y[i] / h[i] - h[i] * z[i] / 6.0,
+                          t_begin: t[i],
+                          t_end: t[i + 1],
+                      })
+    }
+
+    CubicSpline {
+        sections: sections,
+        section_bounds: t,
+    }
+}
+
+fn create_cubic_spline_clamped(pts: &[(f64, f64)]) -> CubicSpline {
+    let der0 = -100.0;
+    let der1 = 200.0;
+
+    let y = pts.iter().map(|&(_, y)| y).collect::<Vec<_>>();
+    let t = pts.iter().map(|&(t, _)| t).collect::<Vec<_>>();
+
+    let n = pts.len();
+    let mut h = Vec::new();
+    let mut b = Vec::new();
+    let mut v = Vec::new();
+    let mut u = Vec::new();
+
+    for i in 0..n - 1 {
+        h.push(t[i + 1] - t[i]);
+        b.push((y[i + 1] - y[i]) / h.last().unwrap());
+    }
+    for i in 1..n - 1 {
+        v.push(2.0 * (h[i - 1] + h[i]));
+        u.push(6.0 * (b[i] - b[i - 1]));
+    }
+
+    let mut real_v = Vec::new();
+    real_v.push(2.0);
+    real_v.extend_from_slice(&v);
+    real_v.push(2.0);
+
+    let mut upper_h = Vec::new();
+    upper_h.push(1.0);
+    //upper_h.extend_from_slice(&h);
+    upper_h.extend_from_slice(&h[1..h.len() - 1]);
+    upper_h.push(1.0);
+
+    let mut lower_h = Vec::new();
+    lower_h.push(1.0);
+    lower_h.extend_from_slice(&h[1..h.len() - 1]);
+    lower_h.push(1.0);
+
+    let mut real_u = Vec::new();
+    real_u.push(6.0 * (b[0] - der0) / h[0]);
+    real_u.extend_from_slice(&u);
+    real_u.push(6.0 * (der1 - b.last().unwrap()) / h.last().unwrap());
+
+    let res = solve_tridiagonal(&lower_h, &real_v, &upper_h, &real_u);
+    let mut z = Vec::new();
+    //z.push(0.5 * (6.0 * b[0] / h[0] - res[0]));
+    z.extend_from_slice(&res);
+    //z.push(-0.5 * (6.0 * b.last().unwrap() / h.last().unwrap() + res.last().unwrap()));
 
     let mut sections = Vec::new();
 
@@ -247,8 +316,11 @@ fn calc_lagrange_polynomial_der2(at_x: f64, pts: &[(f64, f64)]) -> f64 {
 }
 
 fn main() {
-    let x_str = "-2  -1.68421    -1.36842    -1.05263    -0.73684    -0.42105    -0.10526    0.210526    0.526316    0.842105    1.157895    1.473684    1.789474    2.105263    2.421053    2.736842    3.052632    3.368421    3.684211    4";
-    let y_str = "6.880111    5.296874    3.96331 2.891384    2.089794    1.538613    1.148618    0.810363    0.551963    0.492903    0.696817    1.169522    1.899773    2.877061    4.098098    5.577943    7.365993    9.522361    12.00553    14.59995";
+    //let x_str = "-2  -1.68421    -1.36842    -1.05263    -0.73684    -0.42105    -0.10526    0.210526    0.526316    0.842105    1.157895    1.473684    1.789474    2.105263    2.421053    2.736842    3.052632    3.368421    3.684211    4";
+    //let y_str = "6.880111    5.296874    3.96331 2.891384    2.089794    1.538613    1.148618    0.810363    0.551963    0.492903    0.696817    1.169522    1.899773    2.877061    4.098098    5.577943    7.365993    9.522361    12.00553    14.59995";
+
+    let x_str = "0 1 2 3 4";
+    let y_str = "0 3 1 2 2";
 
     let xs = x_str
         .split(' ')
@@ -336,3 +408,8 @@ fn plot_line_and_points(plot_name: &str, line_caption: &str, pt_x: &[f64], pt_y:
     fg.set_terminal("png", &format!("{}.png", plot_name));
     fg.show();
 }
+
+// Reference:
+//http://www.physics.arizona.edu/~restrepo/475A/Notes/sourcea-/node35.html
+//http://cis.poly.edu/~mleung/CS3734/s03/ch07/cubicSpline.pdf
+//https://www.math.ntnu.no/emner/TMA4215/2008h/cubicsplines.pdf
